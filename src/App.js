96fbfc6b483440e89102ccfdbb0e37189f46435a
harshthakur066 <harshthakur066@gmail.com>
2020-04-25 15:32:35 +0530

    added setup and basic view for application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./cmponents/Header";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./AddTransaction";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Balance = () => {
+  return (
+    <div>
+      <h4>Your Balace</h4>
+      <h1>$0.00</h1>
+    </div>
+  );
+};
